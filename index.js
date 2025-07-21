c7a7ee1af491322eef52e01f53f4cbd9ad1ee7ac
aliyapraveen123 <aliyapraveen24@navgurukul.org>
2025-07-21 18:24:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,51 @@ function checkPassword() {
       alert(" Wrong password. Try again!");
     }
   }
+
+
+
+
+
+const riddles = [
+    { question: "Q.1  What's 3 * 7 + 1?", answer: "22" },
+    { question: "Q.2  What's the square root of 144?", answer: "12" },
+    { question: "Q.3  What is 10 + (6 * 2)?", answer: "22" },
+    { question: "Q.4  What's 100 divided by 4?", answer: "25" },
+    { question: "Q.5  What is 5² ?", answer: "25" }
+  ];
+  
+  let index = 0; 
+  
+  
+  function startRiddle() {
+    document.getElementById("quiz-box").style.display = "block"; 
+    document.getElementById("startBtn").style.display = "none"; 
+    showRiddle(); 
+  }
+  
+  
+  function showRiddle() {
+    const current = riddles[index];
+    document.getElementById("question").textContent = current.question;
+    document.getElementById("answer").focus().value = ""; 
+  }
+  
+  
+  function submitAnswer() {
+    const input = document.getElementById("answer").value.trim();
+    const correctAnswer = riddles[index].answer;
+  
+    if (input === correctAnswer) {
+      index++; 
+  
+      if (index < riddles.length) {
+        showRiddle();
+      } else {
+        alert("All riddles solved!");
+        window.location.href = "puzzle3.html"; 
+      }
+    } else {
+      alert("Wrong answer. Try again!");
+    }
+  }
+  
